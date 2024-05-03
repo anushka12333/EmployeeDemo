770badca3f0c0b8e1faa9bb2b077ed16ad24dbda
anushka12333 <71206113+anushka12333@users.noreply.github.com>
2024-05-03 12:38:46 +0530

    Update Employee.java

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Employee {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String empName;
     private String department;
